6c1f2959ab40ad3a1ebca87ee8043855f8a4e660
Oogway <animagig@gmail.com>
2026-06-22 10:11:29 +0900

    6.11 Ladle

---
+++ widgets/.ladle/components.tsx
@@ -0,0 +1,7 @@
+﻿import type { GlobalProvider } from "@ladle/react";
+// @ts-ignore
+import "../src/index.css";
+
+export const Provider: GlobalProvider = ({ children }) => (
+    <div className="bg-surface text-primary">{children}</div>
+);
